ac494d1a767985e24895b030d0b6719a052c2d95
Clay <Clay@10.0.66.195>
2019-10-22 20:37:16 -0500

    comment added

--- MOOCTextEditor/src/document/Document.java
+++ MOOCTextEditor/src/document/Document.java
@@ -20,6 +20,7 @@ public abstract class Document {
 	protected Document(String text)
 	{
 		this.text = text;
+		//test comment
 	}
 	
 	/** Returns the tokens that match the regex pattern from the document 
