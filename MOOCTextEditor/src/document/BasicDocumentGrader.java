615e12cd0e2d1d7379c4592193e847e18d86f3d4
Clay <Clay@10.0.68.147>
2019-11-04 11:32:25 -0600

    comment1

--- MOOCTextEditor/src/document/BasicDocumentGrader.java
+++ MOOCTextEditor/src/document/BasicDocumentGrader.java
@@ -20,7 +20,7 @@ public class BasicDocumentGrader {
                 String result = doc.getNumSentences() + " " + doc.getNumWords() + " " + doc.getNumSyllables() + " ";
                 System.out.print(result);
                 out.print(result);
-                //test comment
+                //test comment1
             }
             out.print("\n");
             out.close();
